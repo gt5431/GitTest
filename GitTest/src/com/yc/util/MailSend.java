6446526398a282d2716d92d3e333054eaf80e615
gt5431 <gt5431@foxmail.com>
2016-05-18 21:21:58 +0800

    create new java source

---
+++ GitTest/src/com/yc/util/MailSend.java
@@ -0,0 +1,15 @@
+package com.yc.util;
+
+public class MailSend {
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+}
